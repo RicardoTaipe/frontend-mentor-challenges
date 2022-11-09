81351cf282788f20d67179b9b9b833224c77f36a
RicardoTaipe <rdavid28@hotmail.es>
2022-11-09 17:38:10 -0500

    fix desktop design

--- intermediate/todo-app-main/index.js
+++ intermediate/todo-app-main/index.js
@@ -4,6 +4,6 @@ darkMode.addEventListener('click', () => {
   const html = document.documentElement;
   html.classList.toggle('dark');
   darkMode.src = html.classList.contains('dark')
-    ? 'images/icon-moon.svg'
-    : 'images/icon-sun.svg';
+    ? 'images/icon-sun.svg'
+    : 'images/icon-moon.svg';
 });
