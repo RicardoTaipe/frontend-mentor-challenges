f748547645cd767e021aa65177633f03d84d36c4
RicardoTaipe <rdavid28@hotmail.es>
2022-11-22 22:48:38 -0500

    label added

--- intermediate/todo-app-main-react/src/components/TodoItem.js
+++ intermediate/todo-app-main-react/src/components/TodoItem.js
@@ -1,15 +1,17 @@
-import iconCross from "../images/icon-cross.svg";
+import iconCross from '../images/icon-cross.svg';
 
 export default function TodoItem({ item }) {
   return (
     <div className="flex justify-between items-center px-5 py-3.5 md:py-[18px]">
+      <label htmlFor="completed"></label>
       <input
         checked={item.completed}
         type="checkbox"
+        id="completed"
         name=""
         className="bg-inherit border-grayish-200 dark:border-blueish-400 p-2.5 rounded-full checked:bg-brand-gradient focus:ring-0 focus:ring-offset-0"
         aria-label={
-          item.completed ? "Mark item as incomplete" : "Mark item as complete"
+          item.completed ? 'Mark item as incomplete' : 'Mark item as complete'
         }
       />
       <p className="flex-1 ml-3">{item.description}</p>
