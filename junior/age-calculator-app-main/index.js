6d7044fa2b114d82b40f628c698d6f673319aa02
Ricardo Taipe <rdavid28@hotmail.es>
2023-06-30 16:12:08 -0500

    ui done for large and small screen

---
+++ junior/age-calculator-app-main/index.js
@@ -0,0 +1,73 @@
+const dayError = document.querySelector("#day-error");
+const monthError = document.querySelector("#month-error");
+const yearError = document.querySelector("#year-error");
+const dayText = document.querySelector("label[for=day]");
+const monthText = document.querySelector("label[for=month]");
+const yearText = document.querySelector("label[for=year]");
+const daysResult = document.querySelector("#days-result");
+const monthsResult = document.querySelector("#months-result");
+const yearsResult = document.querySelector("#years-result");
+
+function isValid(min, max, value) {
+  return value >= min && value <= max;
+}
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const { day: dayInput, month: monthInput, year: yearInput } = form;
+
+  highlightError(dayError, dayText, dayInput, {
+    "The field is required": dayInput.value,
+    "Must be a valid day": isValid(1, 31, dayInput.value),
+  });
+
+  highlightError(monthError, monthText, monthInput, {
+    "The field is required": monthInput.value,
+    "Must be a month": isValid(1, 12, monthInput.value),
+  });
+
+  highlightError(yearError, yearText, yearInput, {
+    "The field is required": yearInput.value,
+    "Must be in the past": new Date().getFullYear() >= Number(yearInput.value),
+  });
+  if (yearInput.value && monthInput.value && dayInput.value) {
+    showResult(`${yearInput.value}-${monthInput.value}-${dayInput.value}`);
+  }
+});
+
+function showResult(date) {
+  const now = moment(); //todays date
+  const end = moment(date); // another date
+  const duration = moment.duration(now.diff(end));
+  if (!duration.isValid) {
+    showError(dayError, dayText, dayInput, "Must be a valid date");
+    showError(monthError, monthText, monthInput, "");
+    showError(yearError, yearText, yearInput, "");
+    return;
+  }
+  yearsResult.innerHTML = duration.years();
+  monthsResult.innerHTML = duration.months();
+  daysResult.innerHTML = duration.days();
+}
+
+function showError(error, text, input, message) {
+  error.classList.remove("hidden");
+  error.innerHTML = message;
+  text.classList.add("text-red");
+  input.classList.add("border-red");
+}
+
+function highlightError(error, text, input, isValid) {
+  for (const message in isValid) {
+    if (!isValid[message]) {
+      showError(error, text, input, message);
+      break;
+    } else {
+      error.classList.add("hidden");
+      text.classList.remove("text-red");
+      input.classList.remove("border-red");
+    }
+  }
+}
---
+++ junior/age-calculator-app-main/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+/**this file is needed for vscode tailwind intellisense */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
