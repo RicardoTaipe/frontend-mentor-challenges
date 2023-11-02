44bc73ff190d0da73381a8f0b0defa96332f2e46
Ricardo Taipe <ricardo.taipe@devsu.com>
2023-11-01 19:09:12 -0500

    deploy to gh-pages

---
+++ junior/advice-generator-app-react/out/_next/static/xzfMUHtPuxd1gz5fT775R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/c8297cc48c521db3.css","static/chunks/pages/index-32ee757b75b8d10a.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
